4e59408c4a05b923ac28ab2a2f5a39ccc4159430
Kim Dong-Hyun <temeddix@gmail.com>
2025-04-14 17:49:25 +0900

    Make the index page work

---
+++ src/test/java/me/temeddix/demo/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package me.temeddix.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
