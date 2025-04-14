1765c0b85c912be1d706b6181151f3f52df58e20
Kim Dong-Hyun <temeddix@gmail.com>
2025-04-14 20:30:07 +0900

    Use application properties

--- src/main/java/me/temeddix/demo/StoreApplication.java
+++ src/main/java/me/temeddix/demo/StoreApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StoreApplication {
     public static void main(String[] args) {
         var context = SpringApplication.run(StoreApplication.class, args);
-        context.getBean(OrderService.class).placeOrder();
+        var orderService = context.getBean(OrderService.class);
+        orderService.placeOrder();
     }
 }
--- src/main/java/me/temeddix/demo/StripePaymentService.java
+++ src/main/java/me/temeddix/demo/StripePaymentService.java
@@ -1,14 +1,33 @@
 package me.temeddix.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service("stripe")
 @Primary
 public class StripePaymentService implements PaymentService {
+    @Value("${stripe.apiUrl}")
+    private String apiUrl;
+
+    @Value("${stripe.enabled}")
+    private boolean enabled;
+
+    @Value("${stripe.timeout:3000}")
+    private int timeout;
+
+    @Value("${stripe.supportedCurrencies}")
+    private List<String> supportedCurrencies;
+
     @Override
     public void processPayment(double amount) {
         System.out.println("STRIPE:");
+        System.out.println("Api url " + apiUrl);
+        System.out.println("Enabled " + enabled);
+        System.out.println("Timeout " + timeout);
+        System.out.println("Supported currencies " + supportedCurrencies);
         System.out.println("Amount " + amount + " paid");
     }
 }
