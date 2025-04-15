3fd11c7ed48d627aae69be492d81ea68ff06d457
Danny Kim <temeddix@gmail.com>
2025-04-15 23:43:43 +0900

    Add the profiles table

---
+++ src/main/resources/db/migration/V4__add_profiles.sql
@@ -0,0 +1,11 @@
+create table
+  `profiles` (
+    `id` int unsigned not null auto_increment,
+    `bio` VARCHAR(255) not null,
+    `phone_number` varchar(255) not null,
+    `date_of_birth` DATETIME not null,
+    `loyalty_points` INT UNSIGNED not null default 0,
+    primary key (`id`),
+    KEY `profiles_relation_1` (`id`),
+    CONSTRAINT `profiles_relation_1` FOREIGN KEY (`id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  );
