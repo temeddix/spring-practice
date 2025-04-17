630846f0769ba1bf05d1b94fd6a3e0d604d9c09c
Danny Kim <temeddix@gmail.com>
2025-04-17 16:32:41 +0900

    Validate DDL on startup

--- src/main/java/me/temeddix/demo/StoreApplication.java
+++ src/main/java/me/temeddix/demo/StoreApplication.java
@@ -1,7 +1,5 @@
 package me.temeddix.demo;
 
-import me.temeddix.demo.entities.Profile;
-import me.temeddix.demo.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StoreApplication {
 
   public static void main(String[] args) {
-    var context = SpringApplication.run(StoreApplication.class, args);
-    var user = User.builder().name("John").email("john@example.com")
-        .password("password").build();
-    var profile = Profile.builder().bio("about myself...").build();
-    user.addProfile(profile);
-    System.out.println(user);
+    SpringApplication.run(StoreApplication.class, args);
   }
 }
