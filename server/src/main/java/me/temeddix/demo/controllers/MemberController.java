74324f3c0ced1a6357adf598c47c662f69ef8080
Danny Kim <temeddix@gmail.com>
2025-07-06 17:38:28 +0900

    Accept PUT /api/members/id

--- server/src/main/java/me/temeddix/demo/controllers/MemberController.java
+++ server/src/main/java/me/temeddix/demo/controllers/MemberController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +31,11 @@ public class MemberController {
     @GetMapping("/{id}")
     public Member get(@PathVariable("id") Long id) {
         return memberRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/{id}")
+    public Member put(@PathVariable("id") Long id, @RequestBody Member member) {
+        member.setId(id);
+        return memberRepository.save(member);
     }
 }
